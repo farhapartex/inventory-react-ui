53b526fb3f90d03ce9a2b25ac3392409c4627771
Md Nazmul Hasan <farhapartex@gmail.com>
2021-09-06 17:49:59 +0600

    registration page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import BasicPage from "./pages/BasicPage";
 import NotFoundPage from "./pages/NotFound";
 import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={BasicPage}></Route>
           <Route exact path="/login" component={LoginPage}></Route>
+          <Route exact path="/registration" component={RegistrationPage}></Route>
           <Route exact path="" component={NotFoundPage} />
         </Switch>
       </Router>
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom";
 import "../style/auth.css";
 
 
-class LoginPage extends Component{
+class RegistrationPage extends Component{
     render(){
         return (
             <div className="auth mx-auto">
                 <form className="">
                 <div className="form-group">
-                        <h3 className="text-center">User Login</h3>
+                        <h3 className="text-center">User Registration</h3>
+                    </div>
+                    <div className="form-group">
+                        <label>First Name</label>
+                        <input type="text" className="form-control" id="username"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input type="text" className="form-control" id="username"/>
                     </div>
                     <div className="form-group">
                         <label>Email</label>
@@ -19,11 +27,15 @@ class LoginPage extends Component{
                         <label>Password</label>
                         <input type="password" className="form-control" id="username"/>
                     </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" for="exampleCheck1">I agree all terms & conditions</label>
+                    </div>
                     <div className="form-group">
                         <button className="btn btn-primary w-100 mt-2">Login</button>
                     </div>
                     <div>
-                        <p>New user? <Link to="/registration">Click to Register</Link></p>
+                        <p>Already user? <Link to="/login">Click to Login</Link></p>
                     </div>
                 </form>
             </div>
@@ -31,4 +43,4 @@ class LoginPage extends Component{
     }
 }
 
-export default LoginPage;
+export default RegistrationPage;
