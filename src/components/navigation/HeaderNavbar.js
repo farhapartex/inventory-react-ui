cdd0105bca4d358c8872c614acd472041824d3d5
Md Nazmul Hasan <farhapartex@gmail.com>
2021-09-08 01:16:42 +0600

    nav list component added

--- src/components/navigation/HeaderNavbar.js
+++ src/components/navigation/HeaderNavbar.js
@@ -1,40 +1,53 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
+import NavLiTag from "../navigation/NavListTag";
 
 
 class HeaderNavBar extends Component{
+    constructor(props){
+        super(props);
+        this.userRole = "admin";
+    }
 
 
     render(){
-        return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand px-3" to="/">Invoice Management System</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        if(this.userRole == "admin"){
+            return (
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <Link className="navbar-brand px-3" to="/">Invoice Management System</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home</a>
-                    </li>
-                    <li claclassNamess="nav-item">
-                        <a className="nav-link" href="#">Product</a>
-                    </li>
-                    <li claclassNamess="nav-item">
-                        <a className="nav-link" href="#">Invocie</a>
-                    </li>
-                    <li claclassNamess="nav-item">
-                        <a className="nav-link" href="#">Settings</a>
-                    </li>
-                    <li claclassNamess="nav-item">
-                        <a className="nav-link" href="#">Md Nazmul Hasan (Owner)</a>
-                    </li>
-                    <li claclassNamess="nav-item">
-                        <Link className="nav-link" to="/login">Logout</Link>
-                    </li>
-                </ul>
-            </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto">
+                            <NavLiTag link="/" itemValue="Home"></NavLiTag>
+                            <NavLiTag link="/" itemValue="Product"></NavLiTag>
+                            <NavLiTag link="/" itemValue="Settings"></NavLiTag>
+                            <NavLiTag link="/" itemValue="Md Nazmul Hasan (Owner)"></NavLiTag>
+                            <NavLiTag link="/login" itemValue="Logout"></NavLiTag>
+                        </ul>
+                    </div>
+                </nav>
+            )
+        }
+
+        else return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link className="navbar-brand px-3" to="/">Invoice Management System</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto">
+                        <NavLiTag link="/" itemValue="Home"></NavLiTag>
+                        <NavLiTag link="/" itemValue="Product"></NavLiTag>
+                        <NavLiTag link="/" itemValue="Settings"></NavLiTag>
+                        <NavLiTag link="/" itemValue="Md Nazmul Hasan (Owner)"></NavLiTag>
+                        <NavLiTag link="/login" itemValue="Logout"></NavLiTag>
+                    </ul>
+                </div>
             </nav>
         )
         
---
+++ src/components/navigation/NavListTag.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+
+
+class NavLiTag extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <li className="nav-item active">
+                <Link className="nav-link" to={this.props.link}>{this.props.itemValue}</Link>
+            </li>
+        )
+        
+    }
+}
+
+export default NavLiTag;
