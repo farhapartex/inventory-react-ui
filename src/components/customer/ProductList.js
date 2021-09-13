3eaab1e44a8b33918a54a8d7f2b17f6c72cb6161
Md Nazmul Hasan <farhapartex@gmail.com>
2021-09-13 17:55:59 +0600

    search box added for product

--- src/components/customer/ProductList.js
+++ src/components/customer/ProductList.js
@@ -23,6 +23,36 @@ class ProductList extends Component{
                     <AnchorTag link="/app/shop/product/create" className="btn btn-warning float-right" itemValue="Create Product"></AnchorTag>
                     <h4>Product List</h4>
                 </div>
+                <div className="row mb-5">
+                    <div className="col-12">
+                        <p><b>Search Box</b></p>
+                    </div>
+                    <div className="col">
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Product name"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="form-group">
+                            <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                                <option selected>Choose Category...</option>
+                                <option value="1">Electronic Accessories</option>
+                                <option value="2">Health & Beauty</option>
+                                <option value="3">Home & Lifestyle</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Product Price"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="form-group">
+                            <input type="submit" className="btn btn-success" value="Search"/>
+                        </div>
+                    </div>
+                </div>
                 <Table className="table table-striped" columnList={this.columnList} tableData={this.tableData} actionLinkPrefix=""></Table>
             </div>
         ) 
