254574ce31ee52818c47e570297a858d26a9907c
Md Nazmul Hasan <farhapartex@gmail.com>
2021-09-17 03:43:00 +0600

    customer password reset form added

---
+++ src/components/profile/PasswordReset.js
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+
+
+class PasswordResetForm extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return (
+            <div className="admin-content mx-auto">
+                <div className="w-100 mb-5">
+                    <h4>Password Reset</h4>
+                </div>
+                <div className="w-75">
+                    <form>
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-4">
+                                    <div className="form-group">
+                                        <label className="mb-2">Old Password</label>
+                                        <input type="password" className="form-control form-control-sm"/>
+                                    </div>
+                                </div>
+                                <div className="col-4">
+                                    <div className="form-group">
+                                        <label className="mb-2">New Password</label>
+                                        <input type="password" className="form-control form-control-sm"/>
+                                    </div>
+                                </div>
+                                <div className="col-4">
+                                    <div className="form-group">
+                                        <label className="mb-2">Confirm Password</label>
+                                        <input type="password" className="form-control form-control-sm"/>
+                                    </div>
+                                </div>
+
+                                <div className="col-12 mt-3">
+                                    <div className="form-group">
+                                        <input type="reset" className="btn btn-warning" value="Reset"/>
+                                        <input type="submit" className="btn btn-success ml-3" value="Submit"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                
+            </div>
+        ) 
+    }
+}
+
+export default PasswordResetForm;
--- src/pages/customer/profile/ProfileBasePage.js
+++ src/pages/customer/profile/ProfileBasePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Switch, Link, useRouteMatch } from "react-router-dom";
 import Profile from "../../../components/profile/Profile";
+import PasswordResetForm from "../../../components/profile/PasswordReset";
 
 
 class ProfileBasePage extends Component{
@@ -14,6 +15,7 @@ class ProfileBasePage extends Component{
             <div className="w-100">
             <Switch>
                 <Route exact path={`${this.props.match.path}/me`} component={Profile}/>
+                <Route exact path={`${this.props.match.path}/password-reset`} component={PasswordResetForm}/>
             </Switch>
             
         </div>
