416d5c95f6d137e768d761f88e8954146d52abd1
Md Nazmul Hasan <farhapartex@gmail.com>
2021-09-18 21:31:27 +0600

    code refactor

--- src/components/navigation/HeaderNavbar.js
+++ src/components/navigation/HeaderNavbar.js
@@ -6,46 +6,33 @@ import NavLiTag from "../navigation/NavListTag";
 class HeaderNavBar extends Component{
     constructor(props){
         super(props);
-        this.userRole = "admin2";
+        this.userRole = "admin";
+        var navbarText = "";
+        if(this.userRole == "admin"){
+            this.navbarText = "Inventory Management (Admin Portal)"
+        }
+        else{
+            this.navbarText = "Inventory Management"
+        }
     }
 
 
     render(){
-        if(this.userRole == "admin"){
             return (
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link className="navbar-brand px-3" to="/">Inventory Management (Admin Portal)</Link>
+                    <Link className="navbar-brand px-3" to="/">{this.navbarText}</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto">
+                        <ul className="navbar-nav ml-auto">
                             <NavLiTag link="/" className="nav-link" itemValue="Home"></NavLiTag>
-                            <NavLiTag link="/admin/settings/role-list" className="nav-link" itemValue="Settings"></NavLiTag>
-                            <NavLiTag link="/" className="nav-link" itemValue="Md Nazmul Hasan (Admin)"></NavLiTag>
                             <NavLiTag link="/login" className="nav-link" itemValue="Logout"></NavLiTag>
                         </ul>
                     </div>
                 </nav>
             )
-        }
-
-        else return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link className="navbar-brand px-3" to="/">Invoice Management System</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <NavLiTag link="/" className="nav-link" itemValue="Md Nazmul Hasan (Owner)"></NavLiTag>
-                        <NavLiTag link="/login" className="nav-link" itemValue="Logout"></NavLiTag>
-                    </ul>
-                </div>
-            </nav>
-        )
         
     }
 }
