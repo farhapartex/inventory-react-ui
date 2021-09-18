18752f2968c21bfab283717ea25f3c9dce4cbd10
Md Nazmul Hasan <farhapartex@gmail.com>
2021-09-18 20:32:03 +0600

    code refactor

--- src/components/customer/EmployeeList.js
+++ src/components/customer/EmployeeList.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import AnchorTag from "../../components/Anchortag";
+import InputFormGroup from "../../components/input/InputFormGroup";
+import SelectFormGroup from "../../components/input/SelectFormGroup";
 import Table from "../../components/table/Table";
 
 
@@ -11,6 +13,12 @@ class EmployeeList extends Component{
             {"id": 1, "name": "Farhan Chowdhury", "email": "farhan.chowdhury@gmail.com", "role": "Sales", "status": "Active", "joined_at": "20th August, 2021"},
             {"id": 2, "name": "David Moree", "email": "david.moree.987@gmail.com", "role": "Sales", "status": "Active", "joined_at": "30th August, 2021"},
         ]
+
+        this.roleData = [
+            {"id": 1, "name": "Admin"},
+            {"id": 2, "name": "Sales"},
+            {"id": 3, "name": "Editor"}
+        ]
     }
 
 
@@ -18,37 +26,26 @@ class EmployeeList extends Component{
         return (
             <div className="admin-content mx-auto">
                 <div className="w-100 mb-5">
-                    <AnchorTag link="/app/shop/employee/create" className="btn btn-warning float-right" itemValue="Create Employee"></AnchorTag>
+                    <AnchorTag link="/app/shop/employee/create" className="btn btn-sm btn-warning float-right" itemValue="Create Employee"></AnchorTag>
                     <h4>Employee List</h4>
                 </div>
                 <div className="row mb-5">
                     <div className="col-12">
                         <p><b>Search Box</b></p>
                     </div>
-                    <div className="col">
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Employee email"/>
-                        </div>
+                    <div className="col-2">
+                        <InputFormGroup labelClassName="mb-2" label="" inputClassName="form-control form-control-sm" placeholder="Employee Email"/>
                     </div>
-                    <div className="col">
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Employee name"/>
-                        </div>
+                    <div className="col-2">
+                        <InputFormGroup labelClassName="mb-2" label="" inputClassName="form-control form-control-sm" placeholder="Employee Name"/>
                     </div>
-                    <div className="col">
-                        <div className="form-group">
-                            <select class="custom-select" id="inlineFormCustomSelectPref">
-                                <option selected>Choose Role...</option>
-                                <option value="1">Electronic Accessories</option>
-                                <option value="2">Health & Beauty</option>
-                                <option value="3">Home & Lifestyle</option>
-                            </select>
-                        </div>
+                    <div className="col-2">
+                        <SelectFormGroup selectClassName="custom-select custom-select-sm" selectData={this.roleData}/>
                     </div>
                     
-                    <div className="col">
+                    <div className="col-2">
                         <div className="form-group">
-                            <input type="submit" className="btn btn-success" value="Search"/>
+                            <input type="submit" className="btn btn-sm btn-success" value="Search"/>
                         </div>
                     </div>
                 </div>
