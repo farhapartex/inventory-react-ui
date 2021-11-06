4cdd3f28e9dffb1eb4c2d1d47e0ba971b339e214
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-06 16:13:13 +0600

    added text form group component

---
+++ src/common/components/textFormGroup/TextFormGroup.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import InputText from "./InputText";
+
+
+class TextFormGroup extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    renderLabel(){
+        if(this.props.label == "" || this.props.label == null){
+            return ""
+        }
+        else{
+            return <label className={this.props.labelClassName}>{this.props.label}</label>
+        }
+        
+    }
+
+    renderInputText(){
+        if(this.props.isReadOnly == true){
+            return <input type="text" className={this.props.inputClassName} placeholder={this.props.placeholder} defaultValue={this.props.value} readOnly/>
+        }
+        else{
+            return <InputText className={this.props.inputClassName} onChange={this.props.onChange} placeholder={this.props.placeholder} value={this.props.value} ref={this.props.ref}/>
+        }
+    }
+
+
+    render(){
+        return (
+            <div className="form-group">
+                {this.renderLabel()}
+                {this.renderInputText()}
+                
+            </div>
+        )
+    }
+}
+
+TextFormGroup.defaultProps = {
+    inputClassName: "form-control",
+    placeholder: "",
+    label: null,
+    value: "",
+    isReadOnly: false
+}
+
+export default TextFormGroup;
